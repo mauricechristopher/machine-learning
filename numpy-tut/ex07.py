e07270e40335d08955693613c58d6f38a40ee83a
mauricechristopher <mchristo@student.42.us.org>
2019-02-06 23:48:11 -0800

    Learning NumPy

--- numpy-tut/ex07.py
+++ numpy-tut/ex07.py
@@ -6,3 +6,14 @@
 # Output:
 # #> [False  True False  True False False  True  True  True  True]
 
+import numpy as np
+
+a = np.random.seed(100)
+a = np.random.randint(0, 5, 10)
+
+out = np.full(a.shape[0], True)                 # Create All True Array
+uniques = np.unique(a, return_index=True)[1]    # Find Unique Elements
+out[uniques] = False                   # Mark Them as False
+
+
+print(out)
