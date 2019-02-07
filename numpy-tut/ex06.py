fe7958cc7257fb21584a27f2419cbe881dc5f495
mauricechristopher <mchristo@student.42.us.org>
2019-02-06 23:35:06 -0800

    Learning NumPy

---
+++ numpy-tut/ex06.py
@@ -0,0 +1,10 @@
+# np.array([1,2,3,np.nan,5,6,7,np.nan])
+
+# Output: 
+# [ 1.  2.  3.  5.  6.  7.]
+
+import numpy as np
+
+arr = np.array([1,2,3,np.nan,5,6,7,np.nan])
+arr = arr[~np.isnan(arr)]
+print(arr)
