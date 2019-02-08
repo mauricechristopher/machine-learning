dbb23fa88a96dbad055a3e528d6f01c438aa33a4
mauricechristopher <mchristo@student.42.us.org>
2019-02-08 04:45:27 -0800

    Learning Pandas

---
+++ pandas-tutorial/ex03.py
@@ -0,0 +1,16 @@
+# Exercise 03: Displaying rows
+
+# Return the first 3 rows of the DataFrame df.
+
+import pandas as pd 
+import numpy as np 
+
+data = {'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat', 'dog', 'dog'],
+        'age': [2.5, 3, 0.5, np.nan, 5, 2, 4.5, np.nan, 7, 3],
+        'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no', 'no']}
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(data=data, index=labels)
+print(df.loc[['a', 'b', 'c']])
