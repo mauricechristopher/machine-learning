395276624ce3203a8ba7b07ebc4bdee4799983a2
mauricechristopher <mchristo@student.42.us.org>
2019-02-08 05:44:34 -0800

    Learning Pandas

---
+++ pandas-tutorial/ex04.py
@@ -0,0 +1,18 @@
+# Exercise 04: Retrieving data
+
+# Select the rows the age is between 2 and 4 (inclusive).
+
+import pandas as pd 
+import numpy as np 
+
+data = {'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat', 'dog', 'dog'],
+        'age': [2.5, 3, 0.5, np.nan, 5, 2, 4.5, np.nan, 7, 3],
+        'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no', 'no']}
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(data=data, index=labels)
+new = df[((df['age'] <= 4) & (df['age'] >= 2))]
+
+print(new)
