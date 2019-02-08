eda83a123296944d74977e5126efe9056a45aff0
mauricechristopher <mchristo@student.42.us.org>
2019-02-08 05:55:06 -0800

    Learning Pandas

---
+++ pandas-tutorial/ex06.py
@@ -0,0 +1,17 @@
+# Exercise 06: Calculate
+
+# Calculate the mean age for each different animal in df.
+
+import pandas as pd 
+import numpy as np 
+
+data = {'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat', 'dog', 'dog'],
+        'age': [2.5, 3, 0.5, np.nan, 5, 2, 4.5, np.nan, 7, 3],
+        'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no', 'no']}
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(data=data, index=labels)
+
+print('Mean Age:', df['age'].mean())
