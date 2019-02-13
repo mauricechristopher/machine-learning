903bd697373c06f76adc713403f4938021057b49
mauricechristopher <mchristo@student.42.us.org>
2019-02-12 17:45:59 -0800

    Learning MPL

---
+++ matplotlib-tutorial/ex00/ex00.py
@@ -0,0 +1,10 @@
+# Exercise 00: Linear graph
+ 
+# Plot this linear graph using matplotlib
+
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.xlabel('some numbers')
+plt.show()
