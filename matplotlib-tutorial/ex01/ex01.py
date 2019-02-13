88a1ef6203d72609b6455c3c96341578bd51fdd8
mauricechristopher <mchristo@student.42.us.org>
2019-02-12 18:02:22 -0800

    Learning MPL

---
+++ matplotlib-tutorial/ex01/ex01.py
@@ -0,0 +1,17 @@
+# Exercise 01: Linear circle graph
+
+# Plot this linear graph using matplotlib with this data
+# import numpy as np
+# x = np.arange(1,11) 
+# y = 2 * x + 5 
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(1,11)
+y = 2 * x + 5
+
+plt.plot(x, y, 'ro')
+plt.ylabel('y axis caption')
+plt.xlabel('x axis caption')
+plt.show()
