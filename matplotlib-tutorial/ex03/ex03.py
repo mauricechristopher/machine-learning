e66712919017f89dad42106ac0169672a4b5f8e2
mauricechristopher <mchristo@student.42.us.org>
2019-02-12 19:04:17 -0800

    Learning MPL

---
+++ matplotlib-tutorial/ex03/ex03.py
@@ -0,0 +1,24 @@
+# Exercise 03: Scatterplot
+# Plot a scatter plot with this data:
+
+# N = 500
+# x = np.random.rand(N)
+# y = np.random.rand(N)
+# colors = (0,0,0)
+# area = np.pi*3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 500
+x = np.random.rand(N)
+y = np.random.rand(N)
+colors = (0,0,0)
+area = np.pi*3
+
+plt.scatter(x, y, s=area, c=colors, alpha=0.5)
+plt.title('Scatter plot')
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.show()
